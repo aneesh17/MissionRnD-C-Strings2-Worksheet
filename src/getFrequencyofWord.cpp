77b520366a013cdc92b003e0cb48350601e86c97
aneesh17 <aneesh17@missionrnd.com>
2016-01-28 23:50:48 +0530

    Submit Commit

--- src/getFrequencyofWord.cpp
+++ src/getFrequencyofWord.cpp
@@ -12,8 +12,49 @@ Note: Dont modify original str or word,Just return count ,Spaces can also be par
 
 #include <stdlib.h>
 
+char * get_SubString(char *str, int i, int j){
+	if (str == NULL || i>j)
+		return NULL;
+	int ResLen = j - i + 1;
+	int index;
+	char* result = (char*)malloc(ResLen - 1);
+	for (index = 0; index < ResLen; index++)
+	{
+		result[index] = str[i++];
+	}
+	result[index] = '\0';
+	return result;
+}
+
+int isEqual(char* str1, char* str2, int len)
+{
+	int index;
+	for (index = 0; index<len; index++)
+	{
+		if (str1[index] != str2[index])
+			return 0;
+	}
+	return 1;
+}
+
 int count_word_in_str_way_1(char *str, char *word){
-	return 0;
+	int index = 0, wrdLen = 0;
+	int before, after, count = 0;
+	while (word[++wrdLen] != '\0')
+	{
+	}
+	//printf("%d\n", wrdLen);
+	before = 0;
+	after = wrdLen - 1;
+	while (str[after] != '\0')
+	{
+		//printf("%s\n", get_SubString(str,before,after));
+		if (isEqual(word, get_SubString(str, before, after), wrdLen))
+			count++;
+		before++;
+		after++;
+	}
+	return count;
 }
 
 int count_word_int_str_way_2_recursion(char *str, char *word){
